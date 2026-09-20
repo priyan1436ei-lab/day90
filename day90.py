e73a6260a1109b90d9403977bdcb1d15ed151dfc
priyan A <priyan1436ei@gmail.com>
2026-09-20 19:06:31 +0530

    Create day90.py

---
+++ day90.py
@@ -0,0 +1,17 @@
+def max_subarray(nums):
+    current_sum = nums[0]
+    max_sum = nums[0]
+
+    for num in nums[1:]:
+        current_sum = max(num, current_sum + num)
+        max_sum = max(max_sum, current_sum)
+
+    return max_sum
+
+
+# Example
+nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+
+result = max_subarray(nums)
+
+print("Maximum Subarray Sum:", result)
